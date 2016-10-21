e2a693ff0c11e1f5944427298d359bc9a664d7cd
nvpmai95 <nvpmai95@gmail.com>
2016-10-22 00:57:50 +0700

    Add read function

--- server.js
+++ server.js
@@ -1,103 +1,38 @@
 #!/usr/bin/env babel-node
 
 require('./helper')
-
-const path = require('path')
 const fs = require('fs').promise
-const Hapi = require('hapi')
-const asyncHandlerPlugin = require('hapi-async-handler')
+const express = require('express')
+const path = require('path')
+const trycatch = require('trycatch')
 
-// const cat = require('./cat')
-// const rm = require('./rm')
-// const mkdir = require('./mkdir')
-// const touch = require('./touch')
+const FILE_DIR = 'path/to'
 
-function getLocalFilePathFromRequest(request) {
-  return path.join(__dirname, 'files', request.params.file)
-}
-
-async function readHandler(request, reply) {
-  const filePath = getLocalFilePathFromRequest(request)
-
-  console.log(`Reading ${filePath}`)
-  const data = await cat(filePath)
-  reply(data)
-}
-
-async function createHandler(request, reply) {
-  /* eslint no-unused-expressions: 0 */
-  const filePath = getLocalFilePathFromRequest(request)
-
-  console.log(`Creating ${filePath}`)
-
-  const stat = await fs.stat(filePath)
-  await stat.isDirectory() ? mkdir(filePath) : touch(filePath)
-  reply()
-}
-
-async function updateHandler(request, reply) {
-  const filePath = getLocalFilePathFromRequest(request)
-
-  console.log(`Updating ${filePath}`)
-  await fs.writeFile(filePath, request.payload)
-  reply()
-}
-
-async function deleteHandler(request, reply) {
-  const filePath = getLocalFilePathFromRequest(request)
-
-  console.log(`Deleting ${filePath}`)
-  await rm(filePath)
-  reply()
-}
-
-async function main() {
-  const port = 8000
-  const server = new Hapi.Server({
-    debug: {
-      request: ['error']
-    }
+async function read(req, res) {
+  const filePath = path.join(__dirname, FILE_DIR, req.url)
+  const data = await fs.readFile(filePath).catch((err) => {
+    res.end(err.message)
   })
-  server.register(asyncHandlerPlugin)
-  server.connection({ port })
-
-  server.route([
-    // READ
-    {
-      method: 'GET',
-      path: '/{file*}',
-      handler: {
-        async: readHandler
-      }
-    },
-    // CREATE
-    {
-      method: 'PUT',
-      path: '/{file*}',
-      handler: {
-        async: createHandler
-      }
-    },
-    // UPDATE
-    {
-      method: 'POST',
-      path: '/{file*}',
-      handler: {
-        async: updateHandler
-      }
-    },
-    // DELETE
-    {
-      method: 'DELETE',
-      path: '/{file*}',
-      handler: {
-        async: deleteHandler
-      }
-    }
-  ])
-
-  await server.start()
-  console.log(`LISTENING @ http://127.0.0.1:${port}`)
+  if (data) {
+    res.end(data.toString())
+  }
 }
 
-main()
+async function initialize() {
+  const app = express()
+  app.use((req, res, next) => {
+    trycatch(next, e => {
+      console.log(e.stack)
+      res.writeHead(500)
+      res.end(e.stack)
+    })
+  })
+
+  const PORT = 8000
+  app.get('*', read)
+
+  app.listen(PORT);
+  console.log(`LISTENING @ http://127.0.0.1:${PORT}`)
+}
+
+initialize()

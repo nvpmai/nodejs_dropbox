c834d40bc68308966a4f6dbc7e981e2db608d179
nvpmai95 <nvpmai95@gmail.com>
2016-10-22 02:49:17 +0700

    Add function setHeaders

--- server.js
+++ server.js
@@ -5,15 +5,29 @@ const fs = require('fs').promise
 const express = require('express')
 const path = require('path')
 const trycatch = require('trycatch')
+const mime = require('mime-types')
 
 const FILE_DIR = 'path/to'
+
+function setHeaders(res, data) {
+  
+  res.setHeader('Content-Length', data.length)
+  res.setHeader('Content-Type', mime.lookup(data))
+}
 
 async function read(req, res) {
   const filePath = path.join(__dirname, FILE_DIR, req.url)
   const data = await fs.readFile(filePath).catch((err) => {
-    res.end(err.message)
+    if (err.code === 'ENOENT') {
+      res.end('File not found')
+    } else if (err.code === 'EISDIR') {
+      res.end('Directory not found')
+    } else {
+      res.end(err.toString())
+    }
   })
   if (data) {
+    setHeaders(res, data)
     res.end(data.toString())
   }
 }
@@ -30,6 +44,7 @@ async function initialize() {
 
   const PORT = 8000
   app.get('*', read)
+  app.head()
 
   app.listen(PORT);
   console.log(`LISTENING @ http://127.0.0.1:${PORT}`)
